b342ad5ef06823ade1f9711817a8e549cb5ec1f0
phuang00 <phuang00@stuy.edu>
2019-11-09 00:02:55 -0500

    fixed random function

--- random.c
+++ random.c
@@ -1,11 +1,15 @@
 #include "random.h"
 
 int rand_num(){
-  int fd = open("/dev/random", O_RDONLY);
+  int fd = open("/dev/random", O_RDWR);
   if (fd < 0){
     printf("%s\n", strerror(errno));
   }
-  int *rand;
-  read(fd, rand, 4);
-  return *rand;
+  int rand;
+  read(fd, &rand, 4);
+  if (errno != 0){
+    printf("%s\n", strerror(errno));
+  }
+  close(fd);
+  return rand;
 }
